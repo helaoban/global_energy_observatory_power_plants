5da65c92dabef6fd32ab2f960f7e64648272e73f
helaoban <carloholl@gmail.com>
2017-01-24 17:56:16 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -51,10 +51,10 @@ def makeNiceKey(value):
 scraperwiki.sqlite.execute("CREATE TABLE IF NOT EXISTS oilports (Name TEXT, Type TEXT, Country TEXT, State TEXT, Type_of_Plant_rng1 TEXT, Type_of_Fuel_rng1_Primary TEXT, Type_of_Fuel_rng2_Secondary TEXT, Design_Capacity_MWe_nbr NUMBER)")
 
 # figure out what's already been downloaded
-knownIDs = scraperwiki.scrape("https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=csv&name=global_energy_observatory_power_plants&query=select%20distinct%20replace(CurrentPage_sys%2C%20'%2Fgeoid%2F'%2C%20'')%20as%20id%20from%20%60oilports%60")
-knownIDs = knownIDs.split("\r\n")
-del knownIDs[0] # first element is the header
-print knownIDs
+# knownIDs = scraperwiki.scrape("https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=csv&name=global_energy_observatory_power_plants&query=select%20distinct%20replace(CurrentPage_sys%2C%20'%2Fgeoid%2F'%2C%20'')%20as%20id%20from%20%60oilports%60")
+# knownIDs = knownIDs.split("\r\n")
+# del knownIDs[0] # first element is the header
+# print knownIDs
 
 # fuelTypes = ["Coal", "Gas", "Oil", "Hydro", "Geothermal", "Nuclear", "Solar_PV", "Solar_Thermal", "Waste", "Wind"]
 fuelTypes = ["Oil_Ports"]
@@ -74,70 +74,70 @@ for fuelType in fuelTypes:
         plantURL = "http://globalenergyobservatory.org/" + link
 
         # only download plants not currently in the database
-        if plantID not in knownIDs:
-            print plantURL
-            try:
-                html = scraperwiki.scrape(plantURL)
-                root = lxml.html.fromstring(html)    
-            except:
-                print "Error downloading " + plantURL
+#         if plantID not in knownIDs:
+        print plantURL
+        try:
+            html = scraperwiki.scrape(plantURL)
+            root = lxml.html.fromstring(html)    
+        except:
+            print "Error downloading " + plantURL
 
-            installationInfo = dict()
-            unitList = list()
-            id = -1
+        installationInfo = dict()
+        unitList = list()
+        id = -1
 
-            inputFields = root.xpath("//input | //select")
-            for inputField in inputFields:
-    
-                #TODO how to deal with checkboxes?
-                typeVal = checkForValue(inputField.xpath("./@type"))
-                idVal = checkForValue(inputField.xpath("./@id"))
-                classVal = checkForValue(inputField.xpath("./@class"))
-                valueVal = checkForValue(inputField.xpath("./@value"))
-                nameVal = checkForValue(inputField.xpath("./@name"))
-                chkVal = checkForValue(inputField.xpath("./@checked"))
-                catVal = checkForValue(inputField.xpath("ancestor::div[h1]/@id"))
-                selOptVal = checkForValue(inputField.xpath("./option[@selected]/@value"))
+        inputFields = root.xpath("//input | //select")
+        for inputField in inputFields:
 
-                if ((typeVal == "text" or typeVal == "hidden") or ((typeVal == "radio" or typeVal == "checkbox") and len(chkVal) > 0)) and len(valueVal) > 0 and len(idVal) > 0:
-                    if idVal == "GEO_Assigned_Identification_Number":
-                        id = valueVal
-                    if catVal <> 'UnitDescription_Block':
-                        installationInfo[makeNiceKey(idVal)] = valueVal
-                    else:  # use different table for unit details to avoid having too many fields in main table
-                        parts = makeNiceKey(idVal).rpartition('_')
-                        while int(parts[2]) > len(unitList):
-                            unitList.append({'GEO_Assigned_Identification_Number':id, 'Unit_Nbr':len(unitList)+1})
-                        unitList[int(parts[2])-1][parts[0]] = valueVal
-                elif len(selOptVal) > 0 and not(selOptVal.startswith("Please Select")) and len(idVal) > 0:
-                    installationInfo[makeNiceKey(idVal)] = selOptVal
+            #TODO how to deal with checkboxes?
+            typeVal = checkForValue(inputField.xpath("./@type"))
+            idVal = checkForValue(inputField.xpath("./@id"))
+            classVal = checkForValue(inputField.xpath("./@class"))
+            valueVal = checkForValue(inputField.xpath("./@value"))
+            nameVal = checkForValue(inputField.xpath("./@name"))
+            chkVal = checkForValue(inputField.xpath("./@checked"))
+            catVal = checkForValue(inputField.xpath("ancestor::div[h1]/@id"))
+            selOptVal = checkForValue(inputField.xpath("./option[@selected]/@value"))
 
-            #add in fuel type
-            #installationInfo["Fuel_type"] = fuelType
+            if ((typeVal == "text" or typeVal == "hidden") or ((typeVal == "radio" or typeVal == "checkbox") and len(chkVal) > 0)) and len(valueVal) > 0 and len(idVal) > 0:
+                if idVal == "GEO_Assigned_Identification_Number":
+                    id = valueVal
+                if catVal <> 'UnitDescription_Block':
+                    installationInfo[makeNiceKey(idVal)] = valueVal
+                else:  # use different table for unit details to avoid having too many fields in main table
+                    parts = makeNiceKey(idVal).rpartition('_')
+                    while int(parts[2]) > len(unitList):
+                        unitList.append({'GEO_Assigned_Identification_Number':id, 'Unit_Nbr':len(unitList)+1})
+                    unitList[int(parts[2])-1][parts[0]] = valueVal
+            elif len(selOptVal) > 0 and not(selOptVal.startswith("Please Select")) and len(idVal) > 0:
+                installationInfo[makeNiceKey(idVal)] = selOptVal
 
-            installationInfo = removeBogusColumnsFromDict(installationInfo)
+        #add in fuel type
+        #installationInfo["Fuel_type"] = fuelType
 
-            # TODO Description_ID does not always correspond to the actual page URL, need to contact Rajan Gupta of GEO as he's probably not aware of this.
-            # select Description_ID, CurrentPage_sys, GEO_Assigned_Identification_Number from `swdata` where Description_ID != GEO_Assigned_Identification_Number OR replace(CurrentPage_sys, "/geoid/", "") != GEO_Assigned_Identification_Number
-            # Using GEO_Assigned_Identification_Number, it corresponds to CurrentPage_sys except in one case
+        installationInfo = removeBogusColumnsFromDict(installationInfo)
 
-            try:
-                #primary key is based on id
-                scraperwiki.sqlite.save(unique_keys=["GEO_Assigned_Identification_Number"], data=installationInfo, table_name="oilports")
-                if len(unitList) > 0:
-                    scraperwiki.sqlite.save(unique_keys=["GEO_Assigned_Identification_Number","Unit_Nbr"], data=unitList, table_name="ppl_units")
-            except:
-                print "Error saving to DB" + ": " + str(sys.exc_info()[1])
+        # TODO Description_ID does not always correspond to the actual page URL, need to contact Rajan Gupta of GEO as he's probably not aware of this.
+        # select Description_ID, CurrentPage_sys, GEO_Assigned_Identification_Number from `swdata` where Description_ID != GEO_Assigned_Identification_Number OR replace(CurrentPage_sys, "/geoid/", "") != GEO_Assigned_Identification_Number
+        # Using GEO_Assigned_Identification_Number, it corresponds to CurrentPage_sys except in one case
 
-            time.sleep(5) #sleep a little to be kind to the server, running into "Temporary failure in name resolution"
-        #else:
-            # add in the fuel type since we forgot to put it in the first time around, it's not actually on the page usually
-            # Nono: Wasn't it already in the Type field? (hidden field within HTML source)
-            # Chris: I think you're correct, and I only noticed this field much later.
+        try:
+            #primary key is based on id
+            scraperwiki.sqlite.save(unique_keys=["GEO_Assigned_Identification_Number"], data=installationInfo, table_name="oilports")
+            if len(unitList) > 0:
+                scraperwiki.sqlite.save(unique_keys=["GEO_Assigned_Identification_Number","Unit_Nbr"], data=unitList, table_name="ppl_units")
+        except:
+            print "Error saving to DB" + ": " + str(sys.exc_info()[1])
 
-            #currentEntry = dict()
-            #currentEntry = scraperwiki.sqlite.execute("select * from swdata where Description_ID = " + str(plantID))
-            #scraperwiki.sqlite.execute("UPDATE swdata SET Fuel_type='" + fuelType + "' WHERE Description_ID=" + str(plantID))
-            #print "UPDATE swdata SET Fuel_type='" + fuelType + "' WHERE Description_ID=" + str(plantID)
-            #currentEntry["Fuel_Type"] = fuelType
-            #scraperwiki.sqlite.save(unique_keys=["Description_ID"], data=currentEntry)
+        time.sleep(5) #sleep a little to be kind to the server, running into "Temporary failure in name resolution"
+    #else:
+        # add in the fuel type since we forgot to put it in the first time around, it's not actually on the page usually
+        # Nono: Wasn't it already in the Type field? (hidden field within HTML source)
+        # Chris: I think you're correct, and I only noticed this field much later.
+
+        #currentEntry = dict()
+        #currentEntry = scraperwiki.sqlite.execute("select * from swdata where Description_ID = " + str(plantID))
+        #scraperwiki.sqlite.execute("UPDATE swdata SET Fuel_type='" + fuelType + "' WHERE Description_ID=" + str(plantID))
+        #print "UPDATE swdata SET Fuel_type='" + fuelType + "' WHERE Description_ID=" + str(plantID)
+        #currentEntry["Fuel_Type"] = fuelType
+        #scraperwiki.sqlite.save(unique_keys=["Description_ID"], data=currentEntry)
